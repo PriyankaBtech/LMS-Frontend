7e48fa3a01817f40cfe7207bb42d4f7dec72ab72
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-11-22 23:54:01 +0530

    Added AboutUs page

---
+++ Frontend_lms/src/Pages/AboutUs.jsx
@@ -0,0 +1,40 @@
+import AboutPageImage from "../Assets/Images/ImageOne.png";
+import HomeLayout from "../Layout/HomeLayout";
+
+function AboutUs() {
+  return (
+    <HomeLayout>
+        <div className="pl-20 pt-20 flex flex-col text-white">
+            <div className="flex items-center gap-5 mx-10">
+                <section className="w-1/2 space-y-10">
+                    <h1 className="text-5xl text-yellow-500 font-semibold">
+                        Quality Education
+                    </h1>
+                    <p className="text-xl text-gray-200">
+                        Our goal is to provide quality education to the world. 
+                        We are providing the platform for the aspiring teachers and students to share
+                        their skills, creativity and knowledge to each other to empower and contribute
+                        in the growth and wellness of mankind.  
+                    </p>
+                </section>
+                
+                <div className="w-1/2">
+                <img
+                    id="test1"
+                    style={{
+                        filter: "drop-shadow(0px 10px 10px rgb(0,0,0));"
+                    }}
+                    alt="about main image"
+                    className="drop-shadow-2xl"
+                    src={ AboutPageImage }
+                />
+                </div>             
+               
+            </div>
+        </div>
+
+    </HomeLayout>  
+  )
+}
+
+export default AboutUs
