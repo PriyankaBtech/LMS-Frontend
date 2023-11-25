95c7d5b68780445e5ffafd60f84dbc9e1456051a
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-11-25 22:39:17 +0530

    Added 404 not found page

---
+++ Frontend_lms/src/Pages/NotFound.jsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
+function NotFound() {
+    const navigate = useNavigate()
+    return (
+        <div className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+            <h1 className="text-9xl font-extrabold text-white tracking-widest">
+                404
+            </h1>
+            <div className="bg-yellow-200 text-black font-semibold px-2 text-sm rounded rotate-12 absolute">
+                Page not found ...
+            </div>
+            <button className="mt-5">
+                <a className="relative inline-block text-sm font-medium text-[#b1c23f] group active:text-yellow-500 focus:outline-none focus:ring">
+                    <span onClick={() => navigate(-1)} className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+                        Go Back
+                    </span>
+                </a>
+            </button>
+        </div>
+    );
+}
+
+export default NotFound;
